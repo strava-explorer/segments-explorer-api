6e23a7c5ff6fb3e202e3b1faeef83fbb55285f4f
Dylan Havelock <dylan.havelock@gmail.com>
2021-10-15 14:28:13 -0400

    Add activity filter

--- src/main/java/com/dhavelock/segmentsexplorerapi/service/StravaApiService.java
+++ src/main/java/com/dhavelock/segmentsexplorerapi/service/StravaApiService.java
@@ -1,6 +1,7 @@
 package com.dhavelock.segmentsexplorerapi.service;
 
 import com.strava.api.ActivitiesApi;
+import com.strava.model.ActivityType;
 import com.strava.model.SummaryActivity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -20,6 +22,11 @@ public class StravaApiService {
         final int now = Long.valueOf(Instant.now().getEpochSecond()).intValue();
         final int nowMinus90Days = Long.valueOf(now - 60 * 60 * 24 * 90).intValue();
 
-        return activitiesApi.getLoggedInAthleteActivities(now, nowMinus90Days, 1, 200);
+        return activitiesApi.getLoggedInAthleteActivities(now, nowMinus90Days, 1, 200)
+                .stream()
+                .filter(activity -> ActivityType.RUN.equals(activity.getType()))
+                .filter(activity -> activity.getMap() != null)
+                .filter(activity -> activity.getMap().getSummaryPolyline() != null)
+                .collect(Collectors.toList());
     }
 }
