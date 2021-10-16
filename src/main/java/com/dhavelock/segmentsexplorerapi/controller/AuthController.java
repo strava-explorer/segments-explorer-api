602f2640f7f518c3c9ad5cd4cd18650c89bb4735
Dylan Havelock <dylan.havelock@gmail.com>
2021-10-16 14:25:40 -0400

    fix vulnerability

--- src/main/java/com/dhavelock/segmentsexplorerapi/controller/AuthController.java
+++ src/main/java/com/dhavelock/segmentsexplorerapi/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.dhavelock.segmentsexplorerapi.controller;
 import com.dhavelock.segmentsexplorerapi.model.AuthTokenDto;
 import com.dhavelock.segmentsexplorerapi.service.StravaAuthorizationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,11 @@ public class AuthController {
     public ResponseEntity<AuthTokenDto> getAuthorizationToken(
             @RequestParam String accessToken
     ) {
+
+        if (!accessToken.matches("[a-zA-Z0-9]*")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
         final String authorizationCode = stravaAuthorizationService.getAuthorizationCode(accessToken);
 
         return ResponseEntity.ok(
